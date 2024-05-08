23ba78cdb4cbcee6948a8576e15571ddeeaf700f
klina8575 <klina8575@gmail.com>
2024-05-08 16:22:17 +0900

    up

--- ch03/src/App.js
+++ ch03/src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
-import MyComponent from './MyComponent'
+// import MyComponent from './MyComponent'
 //import Counter from './Counter'
-import Say from './Say'
+// import Say from './Say'
+import Type from './Type'
 
 function App() {
    // const name = 'react'
@@ -11,8 +12,12 @@ function App() {
    //       자식속성
    //    </MyComponent>
    // )
+
    //return <Counter />
-   return <Say />
+
+   //return <Say />
+
+   return <Type str="react" num={200} bool={1 == 1} arr={[0, 1, 2]} json={{ react: '리액트', time: 2 }} func={alert('Hello function Props!')} />
 }
 
 export default App
